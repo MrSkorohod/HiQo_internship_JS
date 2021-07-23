3855bf440af4884d8c51849974d8dbabcf4d86d1
MrSkorohod <mr.vl.skorohod@gmail.com>
2021-07-23 09:40:42 +0300

    resolve solution

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,6 +1,6 @@
 /* Task 1 */
 function chunk(arr, num) {
-  if (!Array.isArray(arr) || typeof num !== 'number') return [];
+  if (!Array.isArray(arr) || typeof num !== "number") return [];
 
   const result = [];
 
@@ -14,19 +14,19 @@ function chunk(arr, num) {
 /* Task 2 */
 function difference(arr1, arr2) {
   if (!Array.isArray(arr1) || !Array.isArray(arr2)) return [];
-  
-  return arr1.concat(arr2).reduce(( (acc, el) => {
-    if (acc.includes(el)) { 
-        acc.splice(acc.indexOf(el), 1);
-    } else {
-        acc.push(el);
-    }
-    return acc;
-  }), []);
 
-  // return arr1
-  //   .filter((x) => !arr2.includes(x))
-  //   .concat(arr2.filter((x) => !arr1.includes(x)));
+  // return arr1.concat(arr2).reduce((acc, el) => {
+  //   if (acc.includes(el)) {
+  //     acc.splice(acc.indexOf(el), 1);
+  //   } else {
+  //     acc.push(el);
+  //   }
+  //   return acc;
+  // }, []);
+
+  return arr1
+    .filter((x) => !arr2.includes(x))
+    .concat(arr2.filter((x) => !arr1.includes(x)));
 }
 
 /* Task 3 */
@@ -41,13 +41,13 @@ function findIndex(arr, searchParam) {
 
   if (
     !Array.isArray(arr) ||
-    (typeof searchParam !== 'number' &&
-      typeof searchParam !== 'string' &&
-      typeof searchParam !== 'function')
+    (typeof searchParam !== "number" &&
+      typeof searchParam !== "string" &&
+      typeof searchParam !== "function")
   )
-    return 'Invalid arguments';
+    return "Invalid arguments";
 
-  if (typeof searchParam === 'number' || typeof searchParam === 'string') {
+  if (typeof searchParam === "number" || typeof searchParam === "string") {
     return arr.indexOf(searchParam);
   }
   return arr.findIndex(searchParam);
@@ -66,7 +66,7 @@ function fromPairs(arr) {
 
   for (let i = 0; i < arr.length; i += 1) {
     if (!Array.isArray(arr[i]) || arr[i].length !== 2) {
-      return 'Invalid argument';
+      return "Invalid argument";
     }
   }
 
@@ -76,17 +76,17 @@ function fromPairs(arr) {
 /* Task 6 */
 function uniq(arr) {
   if (!Array.isArray(arr) || arr.length === 0) return [];
-  
-  return arr.reduce(( (acc ,el) => {
-    if (!acc.includes(el)) acc.push(el);
-    return acc;
-    }), []);
-  //return Array.from(new Set(arr));
+
+  // return arr.reduce((acc, el) => {
+  //   if (!acc.includes(el)) acc.push(el);
+  //   return acc;
+  // }, []);
+  return Array.from(new Set(arr));
 }
 
 /* Task 7 */
 function every(arr, func) {
-  if (!Array.isArray(arr) || arr.length === 0 || typeof func !== 'function')
+  if (!Array.isArray(arr) || arr.length === 0 || typeof func !== "function")
     return false;
 
   return arr.every(func);
@@ -94,7 +94,7 @@ function every(arr, func) {
 
 /* Task 8 */
 function find(arr, func) {
-  if (!Array.isArray(arr) || arr.length === 0 || typeof func !== 'function')
+  if (!Array.isArray(arr) || arr.length === 0 || typeof func !== "function")
     return null;
 
   return arr.find(func);
@@ -102,7 +102,7 @@ function find(arr, func) {
 
 /* Task 9 */
 function groupBy(arr, func) {
-  if (!Array.isArray(arr) || arr.length === 0 || typeof func !== 'function')
+  if (!Array.isArray(arr) || arr.length === 0 || typeof func !== "function")
     return {};
 
   return arr.reduce((acc, el) => {
@@ -130,7 +130,7 @@ function isEqual(obj1, obj2) {
 
   for (let i = 0; i < arrKeys.length; i += 1) {
     switch (typeof obj1[arrKeys[i]]) {
-      case 'object':
+      case "object":
         if (!isEqual(obj1[arrKeys[i]], obj2[arrKeys[i]])) return false;
         break;
       default:
